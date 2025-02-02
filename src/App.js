e6300fc53c93c20333a2cd41c0ba20cf801e994c
workbird <workbirdservices@gmail.com>
2025-02-02 16:42:51 +0530

    added signup page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Signup from './components/Signup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>My Portfolio</h1>
-      </header>
+    <div>
+      <Signup />
     </div>
   );
 }
